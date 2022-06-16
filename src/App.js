e924e69fc763ff60ec740ee2d150d0b0e1584d04
Xaboule <xaboule@gmail.com>
2022-06-16 12:24:23 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import './App.scss';
+
+import { Canvas, useFrame } from 'react-three-fiber';
+
+const Box = ({ position }) => {
+  const mesh = useRef(null);
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+  return (
+    <mesh ref={mesh} position={position}>
+      <boxBufferGeometry attach='geometry' args={[2, 2, 2]} />
+      <meshStandardMaterial attach='material' color='#F0D4ff' />
+    </mesh>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Canvas colorManagement camera={{ position: [-5, 2, 10], fov: 60 }}>
+        <ambientLight intensity={0.3} />
+        <pointLight position={[-10, 0, -20]} intensity={0.5}/>
+        <pointLight position={[0, -10, 0]} intensity={0.5}/>
+
+<group>
+    <mesh>
+      
+    </mesh>
+
+</group>
+
+        <Box position={[-2, 4, 0]} />
+        <Box position={[1, -2, 3]} />
+        <Box position={[5, 5, -5]} />
+      </Canvas>
+    </>
   );
 }
 
